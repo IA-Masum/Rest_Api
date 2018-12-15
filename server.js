cda14810fefe9926d94d501851ec4795091efa9f
IA Masum <iamasum133@gmail.com>
2018-12-15 17:29:55 +0600

    Add cors middleWare

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 /**************************************/
 
@@ -20,6 +21,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 /***************************************/
 
