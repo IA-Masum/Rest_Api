9bb6be5b88360e2b34d82160d1ad73b6bfb629f2
IA Masum <iamasum133@gmail.com>
2018-12-15 18:31:35 +0600

    Create a model for Contact

---
+++ api/models/ContactMdls.js
@@ -0,0 +1,37 @@
+
+/************* Dependencies ************/
+
+const mongoose = require("mongoose");
+const validTest = require("validator");
+
+/***************************************/
+const Schema = mongoose.Schema;
+
+let ContactSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 4
+  },
+  phone: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    validate: {
+      validator: (v) => {
+        return validTest.isEmail(v);
+      },
+      message: `${v} is not an Email`
+    }
+  }
+});
+
+const Contact = mongoose.model("Contact", ContactSchema);
+
+module.exports = Contact;
